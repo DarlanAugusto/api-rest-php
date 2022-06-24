6d3af3c4f3227a67ae54313c8cc2f7914e3e3328
Darlan Silva <darlansilv4@gmail.com>
2022-06-23 23:10:16 -0300

    Criado UserService com as funções CRUD de usuário

---
+++ App/Services/UserService.php
@@ -0,0 +1,31 @@
+<?php
+    namespace App\Services;
+
+    class UserService {
+
+        public function post() {
+            //
+            // Create
+            //
+        }
+
+        public function get() {
+            //
+            // Read
+            //
+        }
+
+        public function update() {
+            //
+            // Update
+            //
+        }
+
+        public function delete() {
+            //
+            // Delete
+            //
+        }
+    }
+
+?>
