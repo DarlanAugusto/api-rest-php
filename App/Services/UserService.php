59172b2e24919cad9bce957d0194bba0e2965b4d
Darlan Silva <darlansilv4@gmail.com>
2022-06-23 23:33:35 -0300

    Consumindo api na chamada do médoto get

--- App/Services/UserService.php
+++ App/Services/UserService.php
@@ -13,6 +13,7 @@
             //
             // Read
             //
+            return "Opa, deu certo!";
         }
 
         public function update() {
--- public_html/index.php
+++ public_html/index.php
@@ -6,8 +6,19 @@
         $url = explode("/", $_GET['url']);
 
         if($url[0] === "api") {
+            //
+            // Está acessando a api
+            //
+            array_shift($url); // removendo o indice 0 => "api", pois nesse ponto não é mais necessário
+            $service = "App\Services\\" . ucfirst($url[0]) . "Service"; // armazenando o serviço que será executado
+            
+            $method = strtolower($_SERVER["REQUEST_METHOD"]); // guardando método utilizado na requisição
 
-            echo "Está acessando a API.";
+            try {
+                echo call_user_func_array(array(new $service, $method), $url); // utiliza um array pra fazer a chamata de um médoto/função
+            } catch (\Exception $e) {
+                //throw $th;
+            }
         }
     }
     
